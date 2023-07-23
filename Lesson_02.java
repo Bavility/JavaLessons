8e58e2398a767c561f25191b3fa6f923ce3e4a12
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-23 16:03:40 +0200

    Lesson_02.java

--- Lesson_02.java
+++ Lesson_02.java
@@ -1,8 +1,10 @@
 public class Main {
   public static void main(String[] args) {
+    
     String firstName = "Sergejs ";
     String lastName = "Zubakins";
     String fullName = firstName + lastName;
+    
     System.out.println(fullName);  
   }
 }
