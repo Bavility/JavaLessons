a8846401d412691041d865b9e8cd365d313af304
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-23 16:03:58 +0200

    Lesson_01_1.java

--- Lesson_01_1.java
+++ Lesson_01_1.java
@@ -1,5 +1,6 @@
 public class Main {
    public static void main(String[] args) {
+      
      System.out.println("Здравствуйте! Меня зовут Сергей");
      System.out.print("Мне ");
      System.out.print(10+18);
