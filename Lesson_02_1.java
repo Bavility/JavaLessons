7e961286eeb7af6866f2662c79c099cc5034ddf8
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-23 16:03:22 +0200

    Lesson_02_1.java

---
+++ Lesson_02_1.java
@@ -0,0 +1,15 @@
+public class Main {
+   public static void main(String[] args) {
+     
+    int age = 28;
+    int young_age = 17;
+     
+    if(age >= young_age) {
+        System.out.println("Вы совершеннолетний");
+        } else {
+          System.out.println("Вы несовершеннолетний");
+}
+
+   }
+}
+// Sergejs Zubakins (Bavility) 
