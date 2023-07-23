4545e4be15a3bbb8d26d876c666bb7a4e7f0a66f
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-23 16:08:45 +0200

    Lesson_02_2.java

---
+++ Lesson_02_2.java
@@ -0,0 +1,13 @@
+public class Main {
+public static void main(String[] args) {
+
+     double num1 = 50;
+     double num2 = 40;
+     double num3 = 0;
+     double avg = num1 + num2 + num3;
+  
+     System.out.print("К оплате: " +avg);
+     System.out.print(" Euro");
+   }
+}
+// Sergejs Zubakins (Bavility) 
