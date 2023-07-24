74c4e7a5093b53fefaa1d539f61678695c87e97e
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-24 18:39:44 +0200

    Lesson_03_2.java

---
+++ Lesson_03_2.java
@@ -0,0 +1,14 @@
+public class Main {
+public static void main(String[] args) {
+
+            int num1 = 10;
+            int num2 = 15;
+
+            if (num1 > num2) {
+                System.out.println(num1);
+            } else {
+                System.out.println(num2);
+            }
+        }
+    }
+// Sergejs Zubakins (Bavility) 
