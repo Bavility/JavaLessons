a721471733b81a7c2380a67712ffa7711559eda1
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-26 20:21:07 +0200

    Lesson_01_1.java

--- Lesson_01_1.java
+++ Lesson_01_1.java
@@ -1,11 +1,11 @@
 public class Main {
 public static void main(String[] args) {
       
-     System.out.println("Здравствуйте! Меня зовут Сергей");
-     System.out.print("Мне ");
-     System.out.print(10+18);
-     System.out.println(" лет");
-     System.out.println("У моего жизненного пути есть сердце.");
+    System.out.println("Здравствуйте! Меня зовут Сергей");
+    System.out.print("Мне ");
+    System.out.print(10+18);
+    System.out.println(" лет");
+    System.out.println("У моего жизненного пути есть сердце.");
    }
 }
 // Sergejs Zubakins (Bavility) 
