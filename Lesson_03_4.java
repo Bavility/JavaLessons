9d0581ebef21dc8de21063b49a2a356b27654b25
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-26 20:30:25 +0200

    Lesson_03_4.java

--- Lesson_03_4.java
+++ Lesson_03_4.java
@@ -1,19 +1,19 @@
 public class Main {
 public static void main(String[] args) {
 
-        int number = 2; 
+    int number = 2; 
 
-        boolean ev = number % 2 == 0;
-        boolean od = number % 2 != 0;
+    boolean ev = number % 2 == 0;
+    boolean od = number % 2 != 0;
 
-        if (number <= 0) {
-            System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
+    if (number <= 0) {
+    System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
             
-        } else if (ev) {
-            System.out.println("Четное");
-        } else if (od) {
-            System.out.println("Нечетное");
-        }
-    }
+    } else if (ev) {
+    System.out.println("Четное");
+    } else if (od) {
+    System.out.println("Нечетное");
+}
+}
 }
 // Sergejs Zubakins (Bavility) 
