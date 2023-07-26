07b4bf4c866a1b6e811fe3e66cdb90ac7ebab5b2
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-26 20:17:46 +0200

    Lesson_03_5.java

--- Lesson_03_5.java
+++ Lesson_03_5.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
         int age = 0; // Введите свой возраст:
 
         if (age <= 0) {
-            System.out.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
+            System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
 
         } else if (age >= 1 && age <= 14) {
             System.out.println("Ребенок");    
