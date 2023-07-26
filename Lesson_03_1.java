b761486108d5f6489ebfdb77e61f4dcd803e8c40
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-26 20:25:25 +0200

    Lesson_03_1.java

--- Lesson_03_1.java
+++ Lesson_03_1.java
@@ -1,19 +1,19 @@
 public class Main {
 public static void main(String[] args) {
 
-        int number = -5;
+    int number = -5;
 
-        boolean isNegative = number < 0;
-        boolean isPositive = number > 0;
-        boolean isZero = number == 0;
+    boolean isNegative = number < 0;
+    boolean isPositive = number > 0;
+    boolean isZero = number == 0;
 
-        if (isNegative) {
-            System.out.println("Число отрицательное!");
-        } else if (isPositive) {
-            System.out.println("Число положительное!");
-        } else if (isZero) {
-            System.out.println("Число равно нулю!");
-        }
-    }
+    if (isNegative) {
+    System.out.println("Число отрицательное!");
+    } else if (isPositive) {
+    System.out.println("Число положительное!");
+    } else if (isZero) {
+    System.out.println("Число равно нулю!");
+}
+}
 }
 // Sergejs Zubakins (Bavility) 
