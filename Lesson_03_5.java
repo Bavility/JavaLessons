4d9072d2c7a50c34913cf5e85a636e90a8ad08f2
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-07-26 20:31:25 +0200

    Lesson_03_5.java

--- Lesson_03_5.java
+++ Lesson_03_5.java
@@ -1,20 +1,20 @@
 public class Main {
 public static void main(String[] args) {
 
-        int age = 0; // Введите свой возраст:
+    int age = 0;
 
-        if (age <= 0) {
-            System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
+    if (age <= 0) {
+    System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
 
-        } else if (age >= 1 && age <= 14) {
-            System.out.println("Ребенок");    
-        } else if (age >= 15 && age <= 18) {
-            System.out.println("Подросток");
-        } else if (age >= 19 && age <= 59) {
-            System.out.println("Взрослый");
-        } else {
-            System.out.println("Пожилой");
-        }
-    }
+    } else if (age >= 1 && age <= 14) {
+    System.out.println("Ребенок");    
+    } else if (age >= 15 && age <= 18) {
+    System.out.println("Подросток");
+    } else if (age >= 19 && age <= 59) {
+    System.out.println("Взрослый");
+    } else {
+    System.out.println("Пожилой");
+}
+}
 }
 // Sergejs Zubakins (Bavility) 
