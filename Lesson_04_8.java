c02c7ba1272e1c8ae37e4b6b82b0825260787706
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-01 18:02:22 +0200

    Lesson_04_8.java

---
+++ Lesson_04_8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class other3 {
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Введите первое число: ");
+    int num1 = scanner.nextInt();
+
+    System.out.print("Введите второе число: ");
+    int num2 = scanner.nextInt();
+
+    System.out.print("Введите третье число: ");
+    int num3 = scanner.nextInt();
+
+    boolean vozrst = (num1 < num2) && (num2 < num3);
+    boolean ubivam = (num1 > num2) && (num2 > num3);
+
+    boolean result = vozrst || ubivam;
+    System.out.println("Результат: " + result);
+}
+}
