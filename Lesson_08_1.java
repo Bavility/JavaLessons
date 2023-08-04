d8246025e63f375078ba877fb9cb8989e86144e1
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-04 22:37:21 +0200

    Lesson_08_1.java

---
+++ Lesson_08_1.java
@@ -0,0 +1,13 @@
+// Напишите программу, которая будет считать от 10 до 1 в обратном порядке, используя цикл do-while.
+
+public class Main {
+public static void main(String[] args) {
+
+    int count = 10;
+
+    do {
+    System.out.print(count+",");
+    count--;
+    } while (count >= 1);
+}
+}
