fb2c67e631c7f123e4922e919c62ee12621ccada
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-06 21:20:16 +0200

    Lesson_03_3.java

--- Lesson_03_3.java
+++ Lesson_03_3.java
@@ -3,7 +3,7 @@
 public class Main {
 public static void main(String[] args) {
 
-    int number = 5; 
+    int number = 5; // Замените значение на своё
 
     boolean ev = number % 2 == 0;
     boolean od = number % 2 != 0;
