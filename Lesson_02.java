0f4e3c97b84e8ffa1fec6a1b5a7919dd9513b0bc
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-06 20:11:07 +0200

    Lesson_02.java

--- Lesson_02.java
+++ Lesson_02.java
@@ -1,4 +1,7 @@
-// Создайте две переменные типа String, firstName и lastName. Присвойте им ваши имя и фамилию. Создайте третью переменную fullName и используйте эти две переменные, чтобы создать полное имя...."
+/*
+Создайте две переменные типа String, firstName и lastName. Присвойте им ваши имя и фамилию. 
+Создайте третью переменную fullName и используйте эти две переменные, чтобы создать полное имя."
+*/
 
 public class Main {
 public static void main(String[] args) {
@@ -7,6 +10,12 @@ public static void main(String[] args) {
     String lastName = "Zubakins";
     String fullName = firstName + lastName;
     
-    System.out.println(fullName); 
+    System.out.println(fullName); // Выводит "Sergejs Zubakins"
 }
 }
+
+/*
+В этом примере мы объявляем две переменные типа String - firstname и lastname, и присваиваем им значения "Sergejs" и "Zubakins" соответственно. 
+Затем мы создаем третью переменную fullname и используем оператор конкатенации "+" для объединения значений firstname и lastname с пробелом между ними. 
+Результат сохраняется в fullname. Наконец, мы выводим fullname на консоль, и должны получить "Sergejs Zubakins".
+*/
