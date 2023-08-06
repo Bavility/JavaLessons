76b7368b7ecac1e0a47591b0f358f04800de0ad7
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-06 21:58:59 +0200

    Lesson_05_2.java

--- Lesson_05_2.java
+++ Lesson_05_2.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class main {
 public static void main(String[] args) {
 
-Scanner scanner = new Scanner(System.in);
+    // Создаем объект Scanner для чтения ввода с клавиатуры
+    Scanner scanner = new Scanner(System.in);
 
     System.out.print("Введите первое число: ");
     int num1 = scanner.nextInt();
