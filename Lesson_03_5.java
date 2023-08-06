34a824231d79af5fde2a7724ab430705a01859ad
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-06 20:48:48 +0200

    Lesson_03_5.java

--- Lesson_03_5.java
+++ Lesson_03_5.java
@@ -3,11 +3,13 @@
 public class Main {
 public static void main(String[] args) {
 
-    int age = 0;
-
+    int age = 0; // Замените значение на своё
+    
+    // Выводим ошибку
     if (age <= 0) {
     System.err.println("Что-то пошло не так!"); // При 0 или -1 выдает ошибку
-
+        
+    // Выводим результат
     } else if (age >= 1 && age <= 14) {
     System.out.println("Ребенок");    
     } else if (age >= 15 && age <= 18) {
@@ -19,3 +21,7 @@ public static void main(String[] args) {
 }
 }
 }
+
+/*
+Программа запрашивает у пользователя возраст и затем определяет возрастную категорию с помощью условных операторов (if-else). Результат выводится в консоль.
+*/
