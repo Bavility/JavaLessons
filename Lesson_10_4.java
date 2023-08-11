92333a045464d3181aba05ce730f0f7b038163cf
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-11 22:50:23 +0200

    Lesson_10_4.java

---
+++ Lesson_10_4.java
@@ -0,0 +1,16 @@
+// Написать программу, которая возвращает сумму всех элементов массива.
+
+public class Main {
+public static void main(String[] args) {
+  
+    int[] array = {5, 10, 15, 20, 25};
+
+    int sum = 0;
+
+    for (int i=0; i< array.length; i++) {
+    sum += array[i]; 
+    }
+
+    System.out.println("Сумма всех элементов массива: " + sum);
+}
+}
