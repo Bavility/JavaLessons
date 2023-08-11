a1bb6352f157a9b77e61b41d4e8cef5469a71730
Sergejs Zubakins <98712936+Bavility@users.noreply.github.com>
2023-08-11 22:48:05 +0200

    Lesson_10_3.java

---
+++ Lesson_10_3.java
@@ -0,0 +1,43 @@
+/**
+* Написать программу, которая определяет,
+* является ли массив строго возрастающим.
+*/
+
+public class Main {
+public static void main(String[] args) {
+
+    int[] numbers = new int[]{1, 2, 3, 4, 5, 6};
+    boolean result = false; 
+  
+    for (int i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] >= numbers[i + 1]) {
+      
+    
+    } else {
+    result = true;  
+    break;
+    
+    }
+    }
+    if (result) {
+    System.out.println(result);
+    } else {
+    System.out.println(result);
+    }
+
+    int[] aray= new int[]{1, 2, 3, 4 ,5};
+    int length=aray.length;
+    boolean resultCheck=true;
+  
+    for(int i=1;i<=length-1;i++)
+    {
+    if((aray[i]-aray[i-1])<0)
+    {
+    resultCheck = false;
+    break;
+    
+    }
+    }
+    System.out.println(resultCheck);
+}
+}
